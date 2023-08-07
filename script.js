acb8c74cb8a8edb44502e9642679b2d7815769af
subhashkumar7694836791 <subhashkumar7694836791@gmail.com>
2023-08-07 15:11:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// gsap.from("svg",{
+//     opacity:0,
+//     strokeDasharray:300,
+//     strokeDashOffset:0,
+//     duration:4,
+//     delay:.2,
+//     ease:Expo.easeInOut,
+    
+
+//     });
+// gsap.to("svg g ",{
+//   rotate:360
+    
+
+//     });
+// // gsap.from("svg>defs>path g,svg>defs+g>text>textpath polyline",{
